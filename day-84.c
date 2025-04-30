155f992826f45c5ca754c05c108b2a6945917e99
Aman <72264622+amandev8207@users.noreply.github.com>
2025-04-30 13:37:35 +0000

    add daily challenges

---
+++ day-84.c
@@ -0,0 +1,16 @@
+// 1295. Find Numbers with Even Number of Digits
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+
+
+
+
+// int findNumbers(int nums[], int n) {
+//     int ans = 0;
+//     while (((ans+=(int)log10(nums[--n])&1),n));
+//     return ans;
+// }
