4b94ccabfd36f941812c7383a9da622c303ea87e
Aman <72264622+amandev8207@users.noreply.github.com>
2025-05-11 16:31:11 +0000

    add daily challenges

---
+++ day-95.c
@@ -0,0 +1,21 @@
+// 1550. Three Consecutive Odds
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+
+
+
+
+
+// bool threeConsecutiveOdds(int* arr, int arrSize) {
+//     for(int i=0;i<arrSize-2;i++){
+//         if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
